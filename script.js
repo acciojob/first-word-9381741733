3e6629ff836cd82b8c80d39a6217e230dcdbd219
9381741733 <jyothsnabhukya022@gmail.com>
2025-01-05 10:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading and trailing spac
+    str = str.trim();
+    
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the whole string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Otherwise, return the substring up to the first space
+    return str.substring(0, spaceIndex);
 }
-
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
